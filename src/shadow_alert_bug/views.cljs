5bdd6f4cb5ad5f4ccb9d657d78cda0ef66a09ae9
Greg Haskins <greg@manetu.com>
2021-06-11 14:11:07 -0400

    Use alert with shadow 2.10.x
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- src/shadow_alert_bug/views.cljs
+++ src/shadow_alert_bug/views.cljs
@@ -1,13 +1,12 @@
 (ns shadow-alert-bug.views
   (:require [re-frame.core :as re-frame]
             [shadow-alert-bug.subs :as subs]
-            ["@material-ui/core" :refer [AppBar]]))
+            ["@material-ui/core" :refer [AppBar]]
+            ["@material-ui/lab" :refer [Alert]]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:> AppBar {:position "relative" :elevation 0}
       [:p "Shadow Test"]]
-     [:h1
-      "Hello from " @name]
-     ]))
+     [:> Alert "This is an alert"]]))
