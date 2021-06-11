eb3ffe88cf6d7d3feab5ca8927a2aba11f05f6ae
Greg Haskins <greg@manetu.com>
2021-06-11 13:35:58 -0400

    Skeleton project
    Signed-off-by: Greg Haskins <greg@manetu.com>

---
+++ src/shadow_alert_bug/config.cljs
@@ -0,0 +1,4 @@
+(ns shadow-alert-bug.config)
+
+(def debug?
+  ^boolean goog.DEBUG)
---
+++ src/shadow_alert_bug/subs.cljs
@@ -0,0 +1,8 @@
+(ns shadow-alert-bug.subs
+  (:require
+   [re-frame.core :as re-frame]))
+
+(re-frame/reg-sub
+ ::name
+ (fn [db]
+   (:name db)))
