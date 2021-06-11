6c24d08d162a93e81c93682693ca3f57200b4295
Greg Haskins <greg@manetu.com>
2021-06-11 14:00:53 -0400

    Add AppBar
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- src/shadow_alert_bug/views.cljs
+++ src/shadow_alert_bug/views.cljs
@@ -1,12 +1,13 @@
 (ns shadow-alert-bug.views
-  (:require
-   [re-frame.core :as re-frame]
-   [shadow-alert-bug.subs :as subs]
-   ))
+  (:require [re-frame.core :as re-frame]
+            [shadow-alert-bug.subs :as subs]
+            ["@material-ui/core" :refer [AppBar]]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
+     [:> AppBar {:position "relative" :elevation 0}
+      [:p "Shadow Test"]]
      [:h1
       "Hello from " @name]
      ]))
